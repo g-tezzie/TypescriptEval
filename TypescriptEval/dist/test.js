b4a94918cdc102293b565ab180d8d01799113f26
g-tezzie <g.tezzie@gmail.com>
2021-02-21 20:30:57 +0900

    tada-

---
+++ TypescriptEval/dist/test.js
@@ -0,0 +1,7 @@
+"use strict";
+function hoge() {
+    setTimeout(function () { return console.log("hoge"); }, 1000);
+}
+console.log("***");
+hoge();
+//# sourceMappingURL=test.js.map
---
+++ TypescriptEval/src/test.ts
@@ -0,0 +1,6 @@
+function hoge(){
+  setTimeout( ()=> console.log("hoge"), 1000)
+} 
+
+console.log("***")
+hoge()
