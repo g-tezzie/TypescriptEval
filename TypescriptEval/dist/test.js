2657206c8adf9996f013cf81c82cfa4499d84b61
g-tezzie <g.tezzie@gmail.com>
2021-02-21 21:03:49 +0900

    Promise with async/await

--- TypescriptEval/dist/test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function hoge() {
-    setTimeout(function () { return console.log("hoge"); }, 1000);
-}
-console.log("***");
-hoge();
-//# sourceMappingURL=test.js.map
--- TypescriptEval/src/test.ts
+++ TypescriptEval/src/test.ts
@@ -1,6 +1,27 @@
-function hoge(){
-  setTimeout( ()=> console.log("hoge"), 1000)
-} 
+function hoge(str: string): Promise<string> {
+  return new Promise(
+    resolve => {
+      console.log(str)
+      setTimeout(() => { resolve('resolved ' + str) }, 1000)
+    }
+  )
+}
 
-console.log("***")
-hoge()
+async function main() {
+  console.log("***")
+  hoge("await なしで呼び出し1")
+  hoge("await なしで呼び出し1.1")
+  hoge("await なしで呼び出し1.2")
+  hoge("await なしで呼び出し1.3")
+  console.log(await hoge("await 付きで呼び出しA"))
+  await Promise.all(
+    [
+      (async () => console.log(await hoge("await 付きで呼び出しA1")))(),
+      (async () => console.log(await hoge("await 付きで呼び出しA2")))()
+    ]
+  )
+  hoge("await なしで呼び出し2")
+  console.log("***")
+}
+
+main();
